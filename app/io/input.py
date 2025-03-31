bae2154f117fd36301b67136d2b9aa723fc062a6
Yelyzaveta Petrenko <elispetrenko23@gmail.com>
2025-03-31 20:25:50 +0300

    Files output.py and input.py were updated.

---
+++ app/io/input.py
@@ -0,0 +1,12 @@
+
+def input_text_from_console():
+    """Function for entering text from the console"""
+    pass
+
+def read_file_builtin(file_path):
+    """Function to read from a file using Python's built-in capabilities"""
+    pass
+
+def read_file_with_pandas(file_path):
+    """Function to read from a file using the pandas library"""
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,12 @@
+
+def output_text_to_console(text):
+    """Function for outputting text to the console"""
+    pass
+
+def write_to_file_builtin(file_path, content):
+    """Function to write to a file using Python's built-in capabilities"""
+    pass
+
+def write_to_file_with_pandas(file_path, content):
+    """Function to write to a file using the pandas library"""
+    pass
